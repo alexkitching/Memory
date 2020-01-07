c6ad55781a0579b178eab3de4813d1ef85d43fbb
alexkitch <sgtmoody87@gmail.com>
2020-01-07 18:00:10 +0000

    - Refactored Particle System Scenario to CPP

--- Memory/include/ParticleSystemScenario.h
+++ Memory/include/ParticleSystemScenario.h
@@ -32,40 +32,8 @@ public:
 	
 	virtual ~ParticleSystemScenario() {}
 
-	void Run() override
-	{
-		if(m_RunTimeTimer.IsStarted() == false) // Begin
-		{
-			Initialise();
-			m_RunTimeTimer.Start();
-		}
-
-		m_RunTimeTimer.Tick();
-
-		// Update Particle Systems
-		int iTotalParticles = 0;
-		for(auto& ps : m_ParticleSystems)
-		{
-			ps.Update();
-			iTotalParticles += ps.GetParticleCount();
-		}
-
-		LOG("Total Particle Count: %i \n", iTotalParticles);
-		
-		if (m_RunTimeTimer.GetTime() >= m_Config.RunLength) 
-		{
-			m_bComplete = true;
-		}
-	}
-	
-	void Reset() override
-	{
-		m_bComplete = false;
-		m_RunTimeTimer.Stop();
-		
-		m_ParticleSystems.clear();
-	}
-	
+	void Run() override;
+	void Reset() override;
 	bool IsComplete() override { return m_bComplete; }
 
 private:
@@ -92,52 +60,7 @@ private:
 		}
 		~ParticleSystem() {}
 
-		void Update()
-		{
-			m_NextParticleTimer.Tick();
-			
-			// Spawn Particles
-			if((int)m_Particles.size() < m_Config.MaxParticles) // Not Reached Max Particles
-			{
-				if(m_NextParticleTimer.IsStarted() == false) // Ensure Timer is Started
-				{
-					m_NextParticleTimer.Start();
-				}
-
-				float fNextTime = m_NextParticleTimer.GetTime();
-				const float fInterval = 1.f / m_Config.ParticleEmissionRatePerSecond;
-				if (fNextTime >= fInterval) // Timer Elapsed
-				{
-					float fIntervals = fNextTime / fInterval;
-					int SpawnCount = (int)fIntervals;
-					for(int i = 0; i < SpawnCount; ++i)
-					{
-						SpawnParticle();
-					}
-					
-
-					m_NextParticleTimer.SetTime(fIntervals - (float)SpawnCount);
-				}
-			}
-
-			for(auto& p : m_Particles)
-			{
-				p.LiveTime += Time::DeltaTime();
-				p.Position[0] += p.Velocity[0] += Time::DeltaTime();
-				p.Position[1] += p.Velocity[1] += Time::DeltaTime();
-				p.Position[2] += p.Velocity[2] += Time::DeltaTime();
-			}
-
-			for(int i = (int)m_Particles.size() - 1; i >= 0; --i)
-			{
-				Particle* p = &m_Particles[i];
-				if(p->LiveTime >= m_Config.LifeTime)
-				{
-					DestroyParticle(i);
-				}
-			}
-			
-		}
+		void Update();
 
 		int GetParticleCount() const { return (int)m_Particles.size(); }
 		
@@ -163,45 +86,15 @@ private:
 			float LiveTime;
 		};
 
-		void SpawnParticle()
-		{
-			// Push new Particle
-			m_Particles.emplace_back(m_Config.OriginPosition[0], m_Config.OriginPosition[1], m_Config.OriginPosition[2]);
+		void SpawnParticle();
+		void DestroyParticle(int a_pIdx);
 
-			// Stop Timer
-			m_NextParticleTimer.Stop();
-			if ((int)m_Particles.size() < m_Config.MaxParticles) // Still Not Reached Max - Start Timer
-			{
-				m_NextParticleTimer.Start();
-			}
-		}
-
-		void DestroyParticle(int a_pIdx)
-		{
-			m_Particles.erase(m_Particles.begin() + a_pIdx);
-		}
-		
 		Config m_Config;
-		
 		DT_Timer m_NextParticleTimer;
-
 		std::vector<Particle> m_Particles;
 	};
 
-	void Initialise()
-	{
-		while ((int)m_ParticleSystems.size() != m_Config.ParticleSystemsCount)
-		{
-			ParticleSystem::Config config =
-			{
-				m_Config.ParticleSystem.MaxParticles,
-				m_Config.ParticleSystem.ParticleStartCount,
-				m_Config.ParticleSystem.ParticleEmissionRate,
-				 m_Config.ParticleSystem.ParticleLifeTime,
-			};
-			m_ParticleSystems.emplace_back(config);
-		}
-	}
+	void Initialise();
 	
 	Config m_Config;
 	DT_Timer m_RunTimeTimer;
---
+++ Memory/src/ParticleSystemScenario.cpp
@@ -0,0 +1,116 @@
+#include "ParticleSystemScenario.h"
+
+void ParticleSystemScenario::Run()
+{
+	if (m_RunTimeTimer.IsStarted() == false) // Begin
+	{
+		Initialise();
+		m_RunTimeTimer.Start();
+	}
+
+	m_RunTimeTimer.Tick();
+
+	// Update Particle Systems
+	int iTotalParticles = 0;
+	for (auto& ps : m_ParticleSystems)
+	{
+		ps.Update();
+		iTotalParticles += ps.GetParticleCount();
+	}
+
+	LOG("Total Particle Count: %i \n", iTotalParticles);
+
+	if (m_RunTimeTimer.GetTime() >= m_Config.RunLength)
+	{
+		m_bComplete = true;
+	}
+}
+
+void ParticleSystemScenario::Reset()
+{
+	m_bComplete = false;
+	m_RunTimeTimer.Stop();
+	m_ParticleSystems.clear();
+}
+
+void ParticleSystemScenario::ParticleSystem::Update()
+{
+	m_NextParticleTimer.Tick();
+
+	// Spawn Particles
+	if ((int)m_Particles.size() < m_Config.MaxParticles) // Not Reached Max Particles
+	{
+		if (m_NextParticleTimer.IsStarted() == false) // Ensure Timer is Started
+		{
+			m_NextParticleTimer.Start();
+		}
+
+		const float fNextTime = m_NextParticleTimer.GetTime();
+		const float fInterval = 1.f / m_Config.ParticleEmissionRatePerSecond;
+		if (fNextTime >= fInterval) // Timer Elapsed
+		{
+			const float fIntervals = fNextTime / fInterval;
+			const int SpawnCount = (int)fIntervals;
+			for (int i = 0; i < SpawnCount; ++i)
+			{
+				SpawnParticle();
+			}
+
+
+			m_NextParticleTimer.SetTime(fIntervals - (float)SpawnCount);
+		}
+	}
+
+	for (auto& p : m_Particles)
+	{
+		p.LiveTime += Time::DeltaTime();
+		p.Position[0] += p.Velocity[0] += Time::DeltaTime();
+		p.Position[1] += p.Velocity[1] += Time::DeltaTime();
+		p.Position[2] += p.Velocity[2] += Time::DeltaTime();
+	}
+
+	for (int i = (int)m_Particles.size() - 1; i >= 0; --i)
+	{
+		Particle* p = &m_Particles[i];
+		if (p->LiveTime >= m_Config.LifeTime)
+		{
+			DestroyParticle(i);
+		}
+	}
+
+}
+
+void ParticleSystemScenario::ParticleSystem::SpawnParticle()
+{
+	// Push new Particle
+	m_Particles.emplace_back(m_Config.OriginPosition[0], m_Config.OriginPosition[1], m_Config.OriginPosition[2]);
+
+	// Stop Timer
+	m_NextParticleTimer.Stop();
+	if ((int)m_Particles.size() < m_Config.MaxParticles) // Still Not Reached Max - Start Timer
+	{
+		m_NextParticleTimer.Start();
+	}
+}
+
+void ParticleSystemScenario::ParticleSystem::DestroyParticle(int a_pIdx)
+{
+	m_Particles.erase(m_Particles.begin() + a_pIdx);
+}
+
+void ParticleSystemScenario::Initialise()
+{
+	while ((int)m_ParticleSystems.size() != m_Config.ParticleSystemsCount)
+	{
+		ParticleSystem::Config config =
+		{
+			m_Config.ParticleSystem.MaxParticles,
+			m_Config.ParticleSystem.ParticleStartCount,
+			m_Config.ParticleSystem.ParticleEmissionRate,
+			m_Config.ParticleSystem.ParticleLifeTime,
+		{0.f} // Zero Vector
+		};
+		m_ParticleSystems.emplace_back(config);
+	}
+}
+
